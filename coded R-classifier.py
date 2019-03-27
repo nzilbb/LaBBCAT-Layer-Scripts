3524489c294b064b8f2fb0eec6e2b32fbe9639f4
djvill <djvill@ucdavis.edu>
2019-03-27 15:46:00 +1300

    Create coded R-classifier.py

---
+++ coded R-classifier.py
@@ -0,0 +1,42 @@
+# Author: Dan Villarreal, daniel.j.villarreal@gmail.com
+# Date: 27 Mar 2019
+# LaBB-CAT Version: 20190109.1225 
+# Layer Scope: segment
+# Layer Type: Text
+# Layer Alignment: none
+# Assumes Existing Layers: R classifier prob
+# 
+# Maps classifier probabilities to Absent or Present based on threshhold of 0.579065
+
+threshhold = 0.579065
+
+# for each turn in the transcript
+for turn in transcript.list("turns"):
+  if annotator.cancelling: break # cancelled by the user
+  
+  # for each word in the turn 
+  for word in turn.list("transcript"):
+    if annotator.cancelling: break # cancelled by the user   
+    
+    # for each phone in the word
+    for segment in word.list("segments"):
+      # get the "R classifier prob" tag, if any
+      prob = segment.my("R classifier prob")
+      
+      # if there's a classifier probability...
+      if prob is not None:
+        
+        # ...if it's convertible to a float...
+        if isinstance(float(prob.label), float):
+          
+          # ...and if it's at least the threshhold
+          if float(prob.label) >= threshhold:
+            # tag as Present
+            tag = segment.createTag(thisLayer.id, "Present")
+            log("Tagged segment " + segment.label + " (classifier probability " + prob.label + ") with " + tag.label)
+          
+          # if the probability is less than the threshhold
+          else:
+            # tag as Absent
+            tag = segment.createTag(thisLayer.id, "Absent")
+            log("Tagged segment " + segment.label + " (classifier probability " + prob.label + ") with " + tag.label)
